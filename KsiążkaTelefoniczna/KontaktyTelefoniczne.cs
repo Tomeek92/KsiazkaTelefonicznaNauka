d6e97eb0a6e2a446d3df94b73bc6cb61fa177e81
Tomasz Fryckowski <tomaszfryckowski@gmail.com>
2024-01-18 10:41:50 +0100

    Add project files.

---
+++ KsiążkaTelefoniczna/KontaktyTelefoniczne.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KsiążkaTelefoniczna
+{
+    public class KontaktyTelefoniczne 
+    {
+        private string _imie;
+        private string _nazwisko;
+        private string _nrTel;
+
+        public KontaktyTelefoniczne(string imie,string nazwisko,string nrTel)
+        {
+            imie = _imie;
+            nazwisko = _nazwisko;
+            nrTel = _nrTel;
+        }
+        public override string ToString()=> $"Imię: {_imie}\nNazwisko: {_nazwisko}\nNrTel: {_nrTel}";
+
+    }
+}
---
+++ KsiążkaTelefoniczna/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KsiążkaTelefoniczna
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            
+            KontaktyTelefoniczne kontakty = new KontaktyTelefoniczne(" "," "," ");
+            TextBox2.Text = kontakty.ToString();
+            
+        }
+
+    
+            private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+            {
+
+            }
+
+            private void TextBox2_TextChanged(object sender, TextChangedEventArgs e)
+            {
+
+
+
+            }
+
+            private void Button_Click(object sender, RoutedEventArgs e)
+            {
+                string kontakt = TextBox2.Text;
+
+                TextBox1.Text += kontakt + "\n"+ "\n";
+
+                TextBox2.Text = "Imię:\nNazwisko:\nNrTel:";
+             }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            TextBox2.Text = "Imię:\nNazwisko:\nNrTel:";
+        }
+    }
+        
+    
+}
